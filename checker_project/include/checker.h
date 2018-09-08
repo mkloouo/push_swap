1ab03636f6335316a9a5deeca82b6bc2866b42c2
Mykola Odnosumov <modnosum@gmail.com>
2018-09-08 19:54:10 +0300

    Start testing stack functionality in checker

--- checker_project/include/checker.h
+++ checker_project/include/checker.h
@@ -6,11 +6,17 @@
 /*   By: modnosum <modnosum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/16 21:09:32 by modnosum          #+#    #+#             */
-/*   Updated: 2018/09/08 15:12:29 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/09/08 18:51:08 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CHECKER_H
 # define CHECKER_H
+
+# include <ft/conv.h>
+# include <ft/stack.h>
+# include <ft/io.h>
+# include <ft/string.h>
+# include <ft/memory.h>
 
 #endif
--- checker_project/source/checker.c
+++ checker_project/source/checker.c
@@ -6,13 +6,50 @@
 /*   By: modnosum <modnosum@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 15:12:38 by modnosum          #+#    #+#             */
-/*   Updated: 2018/09/08 15:12:46 by modnosum         ###   ########.fr       */
+/*   Updated: 2018/09/08 19:53:17 by modnosum         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <checker.h>
 
-int	main(void)
+void				print_stack_with_msg(t_stack *stack, char const *start_msg,
+					char const *end_msg)
 {
+	t_stack_element	*element;
+
+	if (start_msg)
+		ft_printf("%s\n", start_msg);
+	element = stack->bottom;
+	while (element && element != stack->top)
+	{
+		ft_printf("%s\n", (char*)element->data);
+		element = element->next;
+	}
+	if (element && element == stack->top)
+		ft_printf("%s\n", (char*)element->data);
+	if (end_msg)
+		ft_printf("%s\n", end_msg);
+}
+
+int					main(int ac, char **av)
+{
+	int				i;
+	t_stack			*stack;
+
+	if (ac == 1)
+	{
+		ft_fdprintf(IO_STDERR, "Usage: %s num1 num2 ... numN\n", av[0]);
+		return (1);
+	}
+	stack = ft_create_stack();
+	i = 1;
+	while (av[i])
+	{
+		ft_push_stack(stack, ft_create_stack_element(av[i],
+							ft_strlen(av[i]) + 1));
+		++i;
+	}
+	print_stack_with_msg(stack, "initial first stack", "-\nfirst stack");
+	ft_delete_stack(stack, ft_common_deleter);
 	return (0);
 }
